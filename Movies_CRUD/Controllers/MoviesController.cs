27ea3f3ae8f9e73e85230468dd2a764872e540fb
Abdelrhman Magdey <abdelrhman.magdey.10@gmail.com>
2023-06-15 21:14:22 +0300

    Adding View To the App
    Here we add the view page which will show all movies and we added movies model as well

---
+++ Movies_CRUD/Controllers/MoviesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Movies_CRUD.Models;
+
+namespace Movies_CRUD.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly ApplicationDBContext _dbContext;
+        public MoviesController(ApplicationDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var movies =await _dbContext.Movies.ToListAsync();
+            return View(movies);
+        }
+    }
+}
