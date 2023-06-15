057b3758ef79ab646ad0726b701d097f442c6ba0
Abdelrhman Magdey <abdelrhman.magdey.10@gmail.com>
2023-06-15 16:05:00 +0300

    Initial commit

---
+++ Movies_CRUD/Models/ApplicationDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Movies_CRUD.Models
+{
+    public class ApplicationDBContext:DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        { 
+            
+        }
+    }
+}
---
+++ Movies_CRUD/Models/Genre.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Movies_CRUD.Models
+{
+    public class Genre
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] //Auto generate this value
+        public byte Id { get; set; }
+        public string Name { get; set; }
+    }
+}
